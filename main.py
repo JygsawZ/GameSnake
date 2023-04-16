4dbfbbff6ae55bd05cc84313c1e93c72496b6b54
JygsawZ <julien.burgaud1@gmail.com>
2023-04-16 13:45:25 +0200

    Game Snake V0

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
-import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,10 +24,15 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
-
-
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 screen.exitonclick()
